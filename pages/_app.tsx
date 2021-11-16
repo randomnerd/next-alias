9add14fa74df78aff92a54ed8f62cb487067364f
randomnerd <randomnerd@icloud.com>
2021-11-16 10:50:23 +0300

    wip

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -56,10 +56,22 @@ const useTransitionDirection = () => {
     return reverse
 }
 
+let clientScope: Scope
+
 function MyApp({ Component, pageProps }: AppProps) {
+    const scope = fork({
+        values: {
+            ...(clientScope && serialize(clientScope)),
+            ...pageProps.initialState,
+        },
+    })
+    if (typeof window !== 'undefined') clientScope = scope
+    console.log('scope', serialize(scope))
+
     const rev = useTransitionDirection()
     const router = useRouter()
     return (
+        <Provider value={scope}>
         <div className="App">
             <Container className="text-center">
                 <h1><Link href="/">ALIAS</Link></h1>
@@ -81,6 +93,7 @@ function MyApp({ Component, pageProps }: AppProps) {
                 </motion.div>
             </AnimatePresence>
         </div>
+        </Provider>
     );
 }
 
