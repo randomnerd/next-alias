59fb76e3bda4fda86f4c3c4dedd1d2a42e597f33
randomnerd <randomnerd@icloud.com>
2021-11-20 02:49:37 +0300

    tmp

--- next.config.js
+++ next.config.js
@@ -1,5 +1,34 @@
 /** @type {import('next').NextConfig} */
 
 module.exports = {
+        eslint: { ignoreDuringBuilds: true },
+        optimizeFonts: true,
+        swcMinify: true,
+        // // reactStrictMode: true,
+        generateEtags: true,
+        outputFileTracing: true,
+        experimental: {
+        //   plugins: true,
+        // reactRoot: true,
+        //   optimizeImages: true,
+          optimizeCss: true,
+        //   // reactStrictMode: true,
 
+        },
+        // webpack(config, options) {
+        //   const newConfig = {...config}
+        //   newConfig.plugins.push(new HtmlPlugin())
+        //   newConfig.plugins.push(new Critters({
+        //     // external: true,
+        //     preload: 'swap',
+        //     // fonts: true,
+        //     preloadFonts: true,
+        //     // inlineThreshold: 1024,
+        //   }))
+        //   const cfg = typeof newConfig.webpack === "function"
+        //     ? newConfig.webpack(config, options)
+        //     : newConfig
+        //   console.dir(cfg, { depth: 9 })
+        //   return cfg
+        // }
 };
